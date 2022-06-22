ac97ffc682e6d51b852ffcf277ca3755198d2964
Vladimir Tarassenko <99820518+VladimirTaDev@users.noreply.github.com>
2022-06-21 19:47:36 -0600

    convert __dirname to ES module

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import express from "express";
 const app = express();
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 import mongoose from 'mongoose';
 
@@ -11,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, useUnifiedTopology: true
 });
 
-import  Schema  from 'mongoose';
+import Schema from 'mongoose';
 import bodyParser from "body-parser";
 import controllers from "./mongoDB/controllers.js";
 
@@ -19,6 +23,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
+  console.log(__dirname + "Qsdfdssf")
   res.sendFile(__dirname + '/views/index.html')
 });
 
